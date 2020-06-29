039fabc667b11683fac0de626647f02e6380d6c6
MadhushaniHewagama <hewagamamadhushani@gmail.com>
2020-06-29 16:07:44 +0530

    translaeter file

--- sinhala_songs/sinhala_songs/sinhala_translater.py
+++ sinhala_songs/sinhala_songs/sinhala_translater.py
@@ -1,7 +1,66 @@
 import json
-data=None
-with open('./song_output.json') as fp:
-    data = json.load(fp)
+from googletrans import Translator
+import re
+import os
 
-with open('./sinhala_song_lyrics.json', 'w',encoding="utf-8") as f:
-    json.dump(data, f, ensure_ascii=False)
+translator=Translator()
+
+with open('song_output.json' , encoding='utf-8') as f:
+  objects = json.load(f)
+
+formatted_list= []
+
+def format_multiple(multiple):
+  multiple = multiple.strip().split(',')
+  translated = []
+  for o in multiple:
+    translated.append(translator.translate(o,dest="si").text)
+  if(len(translated)!=1):
+   return ','.join(translated)
+  else: 
+   return translated[0]
+
+def generate_title_to_sinhala(title,title_sinhala):
+  if(title_sinhala == None):
+    return translator.translate (re.split(', |_|-|!|',title)[1],dest='si').text
+  else:
+    return title_sinhala
+
+def generate_title_to_sinhala_english(title):
+  if(title == None):
+    return None
+  else:
+    return song['title'].strip().split('|')[0]
+
+
+def format_song(song,id):
+  obj = {
+    "id": id,
+    "title_sinhala_english" :  generate_title_to_sinhala_english(song['title']),
+    "title_sinhala" : generate_title_to_sinhala(song['title'],song['title_sinhala']),
+    "artist_name" :  format_multiple(song['artist_name']),
+    "genre" : format_multiple(song['genre']),
+    "writer_name" : format_multiple(song['writer_name']),
+    "music" : format_multiple(song['music']),
+    "views" : int(song['views'].replace(",","")),
+    "lyrics" : song['song'].strip()
+  }
+  return obj
+
+c = 0
+for song in objects:
+    c +=1  
+    if(song['title'] == None):
+        c -=1 
+        continue
+    obj = format_song(song,c) 
+    formatted_list.append(obj)
+    
+
+with open('sinhala_song_lyrics.json' ,'w', encoding='utf-8') as outf:
+  json.dump(formatted_list,outf,indent=4,ensure_ascii=False)
+
+
+
+
+
